b2e0570fafe39c82479cce32436284824e3c3e5c
LakChe11 <91863188+LakChe11@users.noreply.github.com>
2023-11-21 13:06:13 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,2 +1,16 @@
 public class main {
+PayPalConfiguration config = new PayPalConfiguration()
+    .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX) // Set your environment
+    .clientId("YOUR_CLIENT_ID"); // Replace with your PayPal Client ID
+
+Intent intent = new Intent(this, PayPalService.class);
+intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+startService(intent);
+
+PayPalPayment payment = new PayPalPayment(new BigDecimal("10.00"), "USD", "Payment for XYZ", PayPalPayment.PAYMENT_INTENT_SALE);
+Intent paymentIntent = new Intent(this, PaymentActivity.class);
+paymentIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+paymentIntent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+startActivityForResult(paymentIntent, PAYPAL_REQUEST_CODE);
+
 }
